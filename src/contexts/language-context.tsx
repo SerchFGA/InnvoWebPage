51460d0955c0d7231eaffb5d8426921990d800fb
Sergio Automatizaciones <sergiog.bot@gmail.com>
2025-09-09 06:20:46 +0000

    make de lenguage spanish the default language on start

--- src/contexts/language-context.tsx
+++ src/contexts/language-context.tsx
@@ -14,7 +14,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
-  const [language, setLanguage] = useState<Language>('en');
+  const [language, setLanguage] = useState<Language>('es');
 
   const t = (key: TranslationKey, data?: InterpolationData) => {
     let text = translations[language][key] || translations['en'][key];
